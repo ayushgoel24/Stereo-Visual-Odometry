38deb145aae1a006b29a8c6f501f5ce73019a8e8
ayushgoel24 <ayush.goel2427@gmail.com>
2023-02-08 00:29:51 -0500

    app constants

---
+++ constants/constants.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <string>
+
+// Constants
+namespace constants {
+
+    // TODO: Need to check how can we add path from ROOT directory
+    const std::string applicationConfigurationFilePath = "";
+}
+
+#endif
